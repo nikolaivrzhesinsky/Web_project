e0e8dc3479d8653e599ca15913d9f86411b1e49a
Николай Зотин <15nk33l49@gmail.com>
2022-04-26 16:49:42 +0300

    BookMarket

---
+++ BookMarketRest/src/main/java/com/example/bookmarketrest/BookMarketRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmarketrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketRestApplication.class, args);
+    }
+
+}
---
+++ BookMarketRest/src/test/java/com/example/bookmarketrest/BookMarketRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmarketrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
