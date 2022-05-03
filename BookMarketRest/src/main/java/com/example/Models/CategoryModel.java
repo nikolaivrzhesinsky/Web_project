fd20f7025a1c0f8c277c29065a503c0fd49fb9bf
Николай Зотин <15nk33l49@gmail.com>
2022-05-03 16:59:43 +0300

    add Models 2.0

--- BookMarketRest/src/main/java/com/example/Models/CategoryModel.java
+++ BookMarketRest/src/main/java/com/example/Models/CategoryModel.java
@@ -1,4 +1,35 @@
 package com.example.Models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="category")
 public class CategoryModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<BookModel> books = new ArrayList<>();
+
+    public CategoryModel(String name, List<BookModel> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public void addBook(BookModel book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
 }
--- BookMarketRest/src/main/java/com/example/Models/ZipcodeModel.java
+++ BookMarketRest/src/main/java/com/example/Models/ZipcodeModel.java
@@ -1,4 +1,29 @@
 package com.example.Models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="zipcode")
 public class ZipcodeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "city_id")
+    private CityModel cityModel;
+
+    public ZipcodeModel(String name, CityModel cityModel) {
+        this.name = name;
+        this.cityModel = cityModel;
+    }
 }
