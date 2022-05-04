786743c6936555dbb07da6704d951964e6d1334a
Николай Зотин <15nk33l49@gmail.com>
2022-05-04 19:05:15 +0300

    add db

---
+++ BookMarketRest/src/main/java/com/example/dto/responseDto/CategoryResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.dto.responseDto;
+
+import lombok.Data;
+
+import java.util.List;
+
+    @Data
+    public class CategoryResponseDto {
+        private Long id;
+        private String name;
+        private List<String> bookNames;
+    }
+
