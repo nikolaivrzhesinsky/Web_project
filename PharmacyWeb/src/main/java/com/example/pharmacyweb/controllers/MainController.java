318c32c2d65c70cfdf4eb6eee034b98d006e008d
Николай Зотин <15nk33l49@gmail.com>
2022-05-22 23:22:37 +0300

    First Step

---
+++ PharmacyWeb/src/main/java/com/example/pharmacyweb/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.example.pharmacyweb.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(){
+
+        return "main-page";
+    }
+}
