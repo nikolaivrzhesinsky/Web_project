6408902b1397735bd206451662ef71c8a5279cf9
Николай Зотин <15nk33l49@gmail.com>
2022-05-22 20:49:28 +0300

    First Step

---
+++ PharmacyWeb/src/main/java/com/example/pharmacyweb/PharmacyWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyWebApplication.class, args);
+    }
+
+}
---
+++ PharmacyWeb/src/main/java/com/example/pharmacyweb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.pharmacyweb.repository;
+
+import com.example.pharmacyweb.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
